b36ff4c2f0f4076f512bc6f9f79b7bc2182345a5
Agon Dise <20200003@student.hcmus.edu.vn>
2024-01-19 18:54:41 +0700

    pull

---
+++ Object Detection.py
@@ -0,0 +1,87 @@
+import cv2
+import numpy as np
+import urllib.request
+import requests
+
+# URL of the camera stream
+url = 'http://192.168.109.47/cam-hi.jpg'
+
+
+# OpenCV VideoCapture
+cap = cv2.VideoCapture(url)
+whT = 320
+confThreshold = 0.5
+nmsThreshold = 0.3
+
+# Class names
+classesfile = 'labels.names'
+classNames = []
+with open(classesfile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+
+# YOLOv3 model configuration
+modelConfig = 'yolov3.cfg'
+modelWeights = 'yolov3.weights'
+net = cv2.dnn.readNetFromDarknet(modelConfig, modelWeights)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+# Function to find and mark objects
+def findObject(outputs, im):
+    hT, wT, cT = im.shape
+    bbox = []
+    classIds = []
+    confs = []
+    found_person = False
+
+    for output in outputs:
+        for det in output:
+            scores = det[5:]
+            classId = np.argmax(scores)
+            confidence = scores[classId]
+            if confidence > confThreshold:
+                w, h = int(det[2] * wT), int(det[3] * hT)
+                x, y = int((det[0] * wT) - w / 2), int((det[1] * hT) - h / 2)
+                bbox.append([x, y, w, h])
+                classIds.append(classId)
+                confs.append(float(confidence))
+
+    indices = cv2.dnn.NMSBoxes(bbox, confs, confThreshold, nmsThreshold)
+
+    for i in indices:
+        box = bbox[i]
+        x, y, w, h = box[0], box[1], box[2], box[3]
+        if classNames[classIds[i]] == 'person':
+            found_person = True
+            cv2.rectangle(im, (x, y), (x + w, y + h), (255, 0, 255), 2)
+            cv2.putText(im, f'{classNames[classIds[i]].upper()} {int(confs[i] * 100)}%', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 255), 2)
+
+    if found_person:
+        cv2.putText(im, "True", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        try:
+            response = requests.post('http://192.168.109.47/alarmon', data={'person_detected': 'True'})
+            print("Response from ESP32-CAM:", response.text)
+        except requests.exceptions.RequestException as e:
+            print("Error sending to ESP32-CAM:", e)
+
+# Main loop for processing the video stream
+while True:
+    img_resp = urllib.request.urlopen(url)
+    imgnp = np.array(bytearray(img_resp.read()), dtype=np.uint8)
+    im = cv2.imdecode(imgnp, -1)
+    sucess, img = cap.read()
+    blob = cv2.dnn.blobFromImage(im, 1 / 255, (whT, whT), [0, 0, 0], 1, crop=False)
+    net.setInput(blob)
+    layernames = net.getLayerNames()
+    outputNames = [layernames[i - 1] for i in net.getUnconnectedOutLayers()]
+
+    outputs = net.forward(outputNames)
+
+    findObject(outputs, im)
+
+    cv2.imshow('Image', im)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
